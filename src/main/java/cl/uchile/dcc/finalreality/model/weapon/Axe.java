c738375cafd6aefbc2cb30d3bec2c8f05b8c4cd2
Yiruzz <arturofks@gmail.com>
2022-09-26 20:49:15 -0300

    Agrega en clase Bow y Axe metodos equals, hashcode y toString

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Axe.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Axe.java
@@ -5,6 +5,27 @@ public class Axe extends AbstractWeapon{
     super(name, weight, damage);
   }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName(), getWeight(), getDamage());
+  }
 
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof final Axe that)) {
+      return false;
+    }
+    return this.hashCode() == that.hashCode()
+        && this.getName().equals(that.getName())
+        && this.getWeight() == that.getWeight()
+        && this.getDamage() == that.getDamage();
+  }
+  @Override
+  public String toString() {
+    return "Axe{name=%s, weight=%d, damage='%d'}".formatted(this.getName(), this.getWeight(), this.getDamage());
+  }
 
 }
--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Bow.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Bow.java
@@ -1,11 +1,33 @@
 package cl.uchile.dcc.finalreality.model.weapon;
 
+import java.util.Objects;
+
 public class Bow extends AbstractWeapon{
 
   public Bow(String name, int weight, int damage) {
     super(name, weight, damage);
   }
 
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName(), getWeight(), getDamage());
+  }
 
-
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof final Bow that)) {
+      return false;
+    }
+    return this.hashCode() == that.hashCode()
+        && this.getName().equals(that.getName())
+        && this.getWeight() == that.getWeight()
+        && this.getDamage() == that.getDamage();
+  }
+  @Override
+  public String toString() {
+    return "Bow{name=%s, weight=%d, damage='%d'}".formatted(this.getName(), this.getWeight(), this.getDamage());
+  }
 }
