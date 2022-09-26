35475dd4a002e48644d8623e1539a9a0ea81c2dd
Yiruzz <arturofks@gmail.com>
2022-09-26 20:43:57 -0300

    Agrega en clase Knife metodos equals, hashcode y toString

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/Knife.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/Knife.java
@@ -1,8 +1,33 @@
 package cl.uchile.dcc.finalreality.model.weapon;
 
+import java.util.Objects;
+
 public class Knife extends AbstractWeapon{
 
   public Knife(String name, int weight, int damage) {
     super(name, weight, damage);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getName(), getWeight(), getDamage());
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof final Knife that)) {
+      return false;
+    }
+    return this.hashCode() == that.hashCode()
+        && this.getName().equals(that.getName())
+        && this.getWeight() == that.getWeight()
+        && this.getDamage() == that.getDamage();
+  }
+  @Override
+  public String toString() {
+    return "Knife{name=%s, weight=%d, damage='%d'}".formatted(this.getName(), this.getWeight(), this.getDamage());
   }
 }
