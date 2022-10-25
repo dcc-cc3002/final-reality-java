f875b4a510975813deb34caca82818405a4c678f
Yiruzz <arturofks@gmail.com>
2022-10-24 23:35:31 -0300

    Se crea directorio para tests y se modifica module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
+
 module finalreality.main {
   requires javafx.controls;
   requires org.jetbrains.annotations;
   requires org.apache.commons.lang3;
+  exports cl.uchile.dcc.finalreality.model.weapon;
 }
